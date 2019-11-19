8809c14961f245f0661658d69e285b48ae63207a
LIting Zhang <litingzhang0910@gmail.com>
2019-11-18 22:07:00 -0600

    Select and format domain from filter list

--- filter_domain.py
+++ filter_domain.py
@@ -1,18 +1,40 @@
 import sys, re, json
 
+"""
+This funciton only get domain in the following format
+eg. 
+google.com
+google.com^
+google.com^$third-party
+
+"""
 def filter(filterList):
-    pattern = "\|\|[a-zA-Z0-9.]+?\^"
+    reg3 = "^\|\|[a-zA-Z0-9./]+?\^\$third-party*$" #third party pattern
+    reg1 = "^\|\|[a-zA-Z0-9./]+?\.(net|com)\^*$" #simple domain
     pre = "*://*."
     lines = filterList.read().splitlines()
     domains = []
     for line in lines:
-        x = re.search(pattern, line)
-        if x:
-            domain = x.group(0)
-            domain = domain[2:-1]
-            domain = pre + domain + "/*"
-            domains.append(domain)
-    with open("domains.js", "w") as js_file:
+        y = re.search(reg3, line)#third party pattern 
+        if y:
+            domain = y.group()[2:]
+            temp = re.search("[a-zA-Z0-9./]+?\^", domain)
+            if temp:
+                domain = temp.group()[:-1]
+                domain = pre + domain + "/*"
+                domains.append(domain)
+        else: #simple domain 
+            x = re.search(reg1, line)
+            if x:
+                domain = x.group()[2:]
+                while(domain[len(domain) - 1] == '^'):
+                    domain = domain[:-1]
+                    if(len(domain) < 2):
+                        break
+                domain = pre + domain + "/*"
+                domains.append(domain)
+
+    with open("domains.js", "w") as js_file:#save to js file
         js_file.write("var blocked_domains = %s;" % json.dumps(domains))
     
 """
@@ -27,6 +49,6 @@ def main(argv):
     filter(file)
 
 
-    
+
 if __name__ == "__main__":
     main(sys.argv)

756795106ec326f94557f2937925d4c6cc1bc1b8
wwb6673 <billybriggs33@gmail.com>
2019-11-21 17:58:25 -0600

    Added pullLists

---
+++ pullLists.js
@@ -0,0 +1,10 @@
+urls = ["http://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
+    "https://www.fanboy.co.nz/r/fanboy-complete.txt",
+    "https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt",
+    "https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt"];
+lists = [];
+for (var i = 0; i < urls.length; ++i) {
+    fetch(urls[i]).then(r => r.text()).then(result => {
+        lists.push(result);
+    });
+}

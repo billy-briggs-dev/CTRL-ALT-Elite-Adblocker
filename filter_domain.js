647b5d7e6337ac052d9f5d21fca4f4ea8862fa98
Katherine Durant <illinoiskath@gmail.com>
2019-11-22 13:48:17 -0600

    Fixing bugs
    still broken for simply domains

--- filter_domain.js
+++ filter_domain.js
@@ -5,28 +5,26 @@
 //returns another array of strings
 function filter(filterlist){
     var whatever = filterlist.split("\n");
-    alert("whatever: " +whatever[0]);
-    var reg3 = "^\|\|[a-zA-Z0-9./]+?\^\$third-party$" //third party pattern
-    var reg1 = "^\|\|[a-zA-Z0-9./]+?\.(net|com)\^$" //simple domain
+    var reg3 = /^\|\|[a-zA-Z0-9./]+?\^\$third-party$/ //third party pattern
+    var reg1 = /^\|\|[a-zA-Z0-9./]+?\.(net|com)\^$/ //simple domain
     var pre = "*://*."
     var domains = [];
 
-    for(line in whatever){
+    whatever.forEach(line => {
+        //alert("line: "+ line);
         y = line.search(reg3); //third party pattern
         if (y != -1){
-            domain = y.group().slice(2);
-            alert("domain: " +domain);
-            temp = domain.search("[a-zA-Z0-9./]+?\^");
-            if(temp){
-                domain = temp.group().slice(0,-1);//????
-                //domain = temp.group()[:-1];
+            domain = line.slice(y+2);
+            temp = domain.search(/[a-zA-Z0-9./]+?\^/);
+            if(temp != -1){
+                domain = domain.slice(temp, domain.indexOf("^"));
                 domain = pre + domain + "/*";
-                domains.append(domain);
+                domains.push(domain);
             }
         }else{ //simple domain 
             x = line.search(reg1);
-            if (x){
-                domain = x.group().slice(2);
+            if (x != -1){
+                domain = line.slice(x+2);
                 //domain = x.group()[2:];
                 while(domain[len(domain) - 1] == '^'){
                     domain = domain.slice(0,-1);
@@ -36,10 +34,11 @@ function filter(filterlist){
                     }
                 }
                 domain = pre + domain + "/*";
-                domains.append(domain);
+                //alert("domain: " +domain);
+                domains.push(domain);
             }
         }
-    }
-    //currently returning 
-    return domains;
+    });
+    //alert("domain[0]: " + domains[0]);
+    return domains;       
 }

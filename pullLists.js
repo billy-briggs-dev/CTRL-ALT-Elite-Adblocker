f8875b20b7d485264912b852564da6d4d933b063
Billy <billybriggs33@gmail.com>
2019-11-29 23:21:24 -0600

    Changed fetch statement

--- pullLists.js
+++ pullLists.js
@@ -2,11 +2,13 @@ var urls = ["http://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
 "https://www.fanboy.co.nz/r/fanboy-complete.txt",
 "https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt",
 "https://easylist-downloads.adblockplus.org/fb_annoyances_full.txt"];
-lists = [];
+const lists = [];
 for (var i = 0; i < urls.length; ++i) {
-    fetch(urls[i])
-    .then(res => res.json())
-    .then(data => lists[i] = data)
-    .then(() => lists[i]
-
+    fetch(urls[i]).then(function(response) {
+        return response.blob
+    }).then(function(blob) {
+        const result = blob;
+        lists[i] = result;
+    });
+    alert(lists[i]);
 }
